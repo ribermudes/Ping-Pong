d035554c918586243fc87183270aa37e0293244c
Rachael Bermudes <bermudesorama@gmail.com>
2016-06-10 13:32:17 -0700

    add scripts and css styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
-  $("form#range").submit(function(event){
+  $("form#number").submit(function(event){
    event.preventDefault();
-   var userInput = parseInt($("#").val());
+   //var userInput = parseInt($("#usernumber").val());
 
-   var input = $ ("input#number").val();
-   var result = []
+// Business Logic
+
+   var inputArray = [];
 
    for ( var i = 0; i<numbers.length; i +1){
-     
-   }
+     if (index % 15 === 0) {
+       inputArray.push("pingpong");
+     } else if (index % 5 === 0) {
+      inputArray.push ("pong");
+    } else if ( index % 3 ==0) {
+      inputArray.push ("ping")
+    }
+    $("#results").show(output);
