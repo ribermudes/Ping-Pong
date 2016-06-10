ccf2983a3ecc7abe6d3a44c475e01c7aac9b3391
Rachael Bermudes <bermudesorama@gmail.com>
2016-06-10 14:18:30 -0700

    rewrite scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-  $("form#number").submit(function(event){
-   event.preventDefault();
-   //var userInput = parseInt($("#usernumber").val());
 
 // Business Logic
+var   = function(userNumber) {
 
    var inputArray = [];
 
-   for ( var i = 0; i<numbers.length; i +1){
+   for ( var i = 0; i<userNumber.length; i +1){
      if (index % 15 === 0) {
        inputArray.push("pingpong");
      } else if (index % 5 === 0) {
       inputArray.push ("pong");
     } else if ( index % 3 ==0) {
       inputArray.push ("ping")
-    }
-    $("#results").show(output);
+}
+  return inputArray;
+    debugger;
+
+// Front End Logic
+    $("form#number").submit(function(event){
+     event.preventDefault();
+
+     $("ul.results li").remove();
+     var results = parseInt($("#usernumber").val());
+     $(".results").show();
+
+});
+}
+});
